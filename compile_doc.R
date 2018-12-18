a812df79b576a43f4f17402a7fba835ce75566c0
johnwoodill <johnwoodill@gmail.com>
2018-12-17 21:38:34 -0700

    init commit

---
+++ compile_doc.R
@@ -0,0 +1,22 @@
+pwd <- getwd()
+setwd(pwd)
+
+# Header file with abstract
+top <- paste0(pwd, "/header.tex")
+
+# Document text
+doc <-  paste0(pwd, "/notes.txt")
+
+# Merge config file
+system("cat header.tex notes.txt > Notes-Hands-On-ML-with-scikit-learn-and-TF.tex")
+
+write(paste0("\\end{document}"), "Notes-Hands-On-ML-with-scikit-learn-and-TF.tex", append = TRUE)
+
+# Compile pdf
+system("pdflatex Notes-Hands-On-ML-with-scikit-learn-and-TF.tex")
+
+# Remove Latex Files
+system("rm Notes-Hands-On-ML-with-scikit-learn-and-TF.tex")
+system("rm Notes-Hands-On-ML-with-scikit-learn-and-TF.log")
+system("rm Notes-Hands-On-ML-with-scikit-learn-and-TF.aux")
+# system("rm Notes-Intro-to-Stat-Learning.toc")
